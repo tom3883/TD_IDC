33f1dcfe96e7e4d4d89cdc4a36432169a0c4cd79
Thomas Paul <thomaspaul987@gmail.com>
2024-01-20 16:01:42 +0100

    ADD corese server tests

---
+++ projet_final/app/backend/test-server.py
@@ -0,0 +1,49 @@
+import json
+import pandas as pd
+from SPARQLWrapper import JSON, POST, POSTDIRECTLY, SPARQLWrapper
+
+
+def sparql_service_update(service, update_query):
+    """
+    Helper function to update (DELETE DATA, INSERT DATA, DELETE/INSERT) data.
+
+    """
+    sparql = SPARQLWrapper(service)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    result = sparql.query()
+
+    processed_results = json.load(result.response)
+    cols = processed_results['head']['vars']
+
+    out = []
+    for row in processed_results['results']['bindings']:
+        item = []
+        for c in cols:
+            item.append(row.get(c, {}).get('value'))
+        out.append(item)
+
+    print(out)
+
+    return 'Done'
+
+
+query = '''
+PREFIX : <http://recipes-project.com/schema#>
+
+SELECT ?recipeName ?dishType ?ingredient ?productName
+WHERE {
+    ?product :productName ?productName .
+    SERVICE <http://localhost/service/edamam/findRecipes?keyword=cake> {
+        ?r :name ?recipeName ;
+           :recipeCategory ?dishType ;
+           :recipeIngredient ?ingredient .
+    }
+    FILTER (STR(?productName) = STR(?ingredient))
+}
+'''
+
+wds_Corese = 'http://localhost:8080/sparql'
+
+df = sparql_service_update(wds_Corese, query)
+print(df)
--- projet_final/app/readme.md
+++ projet_final/app/readme.md
@@ -1,5 +1,8 @@
 ## Running the app
 
+Launch Corese server :
+java -jar corese-server-4.5.0.jar -lp -pp "corese_profile.ttl"
+
 To run the backend from command line : 
 1. cd backend
 2. uvicorn main:app --reload
